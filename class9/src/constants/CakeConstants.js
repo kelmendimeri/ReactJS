206943f2851ffbf0a78bf7dfb5d5c52e70cf884d
kelmendimeri <kelmendimeri@gmail.com>
2022-01-15 22:20:42 +0100

    Delete class9 directory

--- class9/src/constants/CakeConstants.js
+++
@@ -1,4 +0,0 @@
-export const BUY_CAKE = "BUY_CAKE";
-export const ADD_CAKE = "ADD_CAKE";
-export const REDUCE_CAKE = "REDUCE_CAKE";
-export const RESTORE_CAKES = "RESTORE_CAKES";
--- class9/src/reducer/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from "redux";
-import CakeReducer from "./CakeReducer";
-import CommentsReducer from "./CommentsReducer";
-
-export default combineReducers({ CakeReducer, CommentsReducer });
